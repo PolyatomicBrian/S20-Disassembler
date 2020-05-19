82b3f1c7cbe636c338f01f9bfa79129f767e77f6
Brian Jopling <brj424@yahoo.com>
2020-05-19 15:10:15 -0400

    Implement opcode & subopcode translation

--- s20-disassembler/disas.py
+++ s20-disassembler/disas.py
@@ -21,7 +21,8 @@ IS_DEBUG = True
 S20_OUTPUT_FILE = "sample.hex"
 
 S20_BITS = 24
-S20_BYTES = 6
+# 6 nibbles (4 bits * 6 nibbles = 24 bits) per instruction
+S20_NUM_INSTR_NIBBLES = 6
 
 INSTR_SET = {
     "OP_CODE": {
@@ -122,7 +123,7 @@ INSTR_SET = {
                 "10": {
                     "INSTRUCTION": "rts"
                 },
-                "1f": {
+                "1F": {
                     "INSTRUCTION": "halt"
                 },
             }
@@ -180,26 +181,64 @@ def print_debug(msg):
 
 
 def get_instruction(op_code, sub_op_code=None):
+    """Lookup corresponding assembly language instr given an opcode and sub opcode"""
     has_sub_op_code = list(INSTR_SET["OP_CODE"][op_code]["SUB_OP_CODE"].keys())[0] is not None
     if not has_sub_op_code:
         return INSTR_SET["OP_CODE"][op_code]["SUB_OP_CODE"][None]["INSTRUCTION"]
     else:
-        sub_op_code = "0" + sub_op_code  # Not correct impl, testing.
-        return INSTR_SET["OP_CODE"][op_code]["SUB_OP_CODE"][sub_op_code]["INSTRUCTION"]
+        return INSTR_SET["OP_CODE"][op_code]["SUB_OP_CODE"][sub_op_code.upper()]["INSTRUCTION"]
+
+
+def hex_to_binary(str_hex):
+    """Convert hexadecimal string to binary string"""
+    str_bin = bin(int(str_hex, 16))[2:]
+    str_bin = str_bin.zfill(24)  # Pad with zeroes until 24 chars long
+    return str_bin
+
+
+def binary_to_hex(str_bin):
+    """Convert binary string to hexadecimal string"""
+    return hex(int(str_bin, 2))[2:]
+
+
+def get_op_code(bits):
+    """Opcode defined as first 4 bits of binary"""
+    op_code_bits = 4
+    bin_op_code = bits[0:op_code_bits]
+    hex_op_code = binary_to_hex(bin_op_code)
+    return hex_op_code
+
+
+def get_sub_op_code(op_code, bits):
+    """Sub Opcode defined as last 5 bits of binary, if opcode is 0"""
+    # Lookup instr by op_code, see if it can have a sub opcode.
+    # Instead of doing the lookup, I could have just checked (if opcode == 0),
+    #  but this will work even if non-zero opcodes are given sub opcodes in the future.
+    has_sub_op_code = list(INSTR_SET["OP_CODE"][op_code]["SUB_OP_CODE"].keys())[0] is not None
+    sub_op_code = None
+    if has_sub_op_code:
+        sub_op_code_bits = 5  # Last 5 bits of binary
+        bin_sub_op_code = bits[-sub_op_code_bits:]  # Get the last 5 bits of the binary str
+        sub_op_code = binary_to_hex(bin_sub_op_code).zfill(2)  # Pad to two chars
+    return sub_op_code
 
 
 def translate_bits(s20_output):
-    # Iterate over 24 bits (6 bytes) at a time.
-    for i in range(0, len(s20_output), S20_BYTES):
-        cur_bytes = s20_output[i:i + S20_BYTES]
-        op_code = cur_bytes[0]
-        sub_op_code = cur_bytes[S20_BYTES-1]
+    """Main process for translating the bits to assembled code"""
+    # Iterate over 24 bits (6 chars) at a time.
+    for i in range(0, len(s20_output), S20_NUM_INSTR_NIBBLES):
+        # Get instructions, 6 nibbles of the S20's output at a time.
+        cur_instr = s20_output[i:i + S20_NUM_INSTR_NIBBLES]
+        # Convert hex to raw binary bits.
+        bits = hex_to_binary(cur_instr)
+        op_code = get_op_code(bits)
+        sub_op_code = get_sub_op_code(op_code, bits)
         instr = get_instruction(op_code, sub_op_code)
         print("INSTR %s" % instr)
 
 
 def main():
-    """Driver"""
+    """Program driver"""
     file_service = FileService()
     s20_output = file_service.get_content_from_file()
     translate_bits(s20_output)
