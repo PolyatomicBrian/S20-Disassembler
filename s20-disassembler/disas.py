70c0f05da5d6d83e2d08ee08e04c35ab3911ad93
Brian Jopling <brj424@yahoo.com>
2020-05-25 21:17:05 -0400

    Change unreachable instructions to data spaces

--- s20-disassembler/disas.py
+++ s20-disassembler/disas.py
@@ -156,7 +156,7 @@ class FileService:
         """Parses content of S20 output file."""
         try:
             file = open(S20_OUTPUT_FILE, "r")
-            content = file.read().replace(" ", "")
+            content = file.read().replace(" ", "").replace("\n", "")
             file.close()
         except:
             error_quit("Failed to read from file %s" % S20_OUTPUT_FILE, 403)
@@ -324,7 +324,7 @@ def build_btree(btree, instrs):
     addr = instrs[0][0]
     btree[addr] = {}
     # Recursive base case: halt implies we're at the end of a branch.
-    if instr == "halt":
+    if instr == "halt" or instr == "rts":
         btree[addr]["LEFT"] = None
         btree[addr]["RIGHT"] = None
         return btree
@@ -347,7 +347,7 @@ def build_btree(btree, instrs):
             else:
                 instrs.pop(0)
         return btree
-    # Else not a halt nor a branch, so tack next instrs onto left child and keep going.
+    # Else not a halt/rts nor a branch, so tack next instrs onto left child and keep going.
     else:
         instrs.pop(0)
         btree[addr]["LEFT"] = build_btree({}, instrs)
@@ -378,7 +378,7 @@ def translate_bits(s20_output):
         operands = get_operands(bits, op_code, sub_op_code, instr)
         operands = ", ".join(operands)
 
-        interp = "\t\t"
+        interp = "        "
         instrs.append([str(hex(addr))[2:].zfill(4), cur_instr, interp, instr, operands])
 
         # Next line
@@ -412,23 +412,32 @@ def label_data(node, instrs):
         # Symbol names are d#, e.g. d0 or d1 or d2 or ...
         label = 'd' + str(data_count)
         # If data already exists at that address, reference that already-existing label.
-        if data_instr[2] != "\t\t":
-            label = data_instr[2]
-        # Fix format of operands depending on whether instruction is ld or st.
-        if instr[3] == "ld":
-            instr[4] = label + ", " + instr[4].split(",")[0]
-        elif instr[3] == "st":
-            instr[4] = instr[4].split(",")[0] + ", " + label
-        # Add symbol to the data address, overwrite existing (unused) instruction with "data".
-        data_instr[2] = label.ljust(8)
-        data_instr[3] = "data"
-        data_instr[4] = str(0)
-        data_count += 1
+        try:
+            if data_instr[2] != "        ":
+                label = data_instr[2]
+            # Fix format of operands depending on whether instruction is ld or st.
+            if instr[3] == "ld":
+                instr[4] = label + ", " + instr[4].split(",")[0]
+            elif instr[3] == "st":
+                instr[4] = instr[4].split(",")[0] + ", " + label
+            # Add symbol to the data address, overwrite existing (unused) instruction with "data".
+            data_instr[2] = label.ljust(8)
+            data_instr[3] = "data"
+            data_instr[4] = str(0)
+            data_count += 1
+        except TypeError:
+            label = 'd' + str(data_count)
+            if instr[3] == "ld":
+                instr[4] = label + ", " + instr[4].split(",")[0] + " [" + label + " references unknown memory address!]"
+            elif instr[3] == "st":
+                instr[4] = instr[4].split(",")[0] + ", " + label + " [" + label + " references unknown memory address!]"
+            print_debug("Unable to find memory address %s" % data_addr)
+            data_count += 1
     if instr[3] in INSTR_WITH_BRANCH:
         label = "skip" + str(skip_count)
         jmp_addr = lookup_data_addr(instr).zfill(4)
         jmp_instr = lookup_instr_by_addr(jmp_addr, instrs)
-        if jmp_instr[2] != "\t\t":
+        if jmp_instr[2] != "        ":
             label = jmp_instr[2]
         instr[4] = instr[4].split(",")[0] + ", " + label
         jmp_instr[2] = label.ljust(8)
@@ -442,10 +451,30 @@ def label_data(node, instrs):
         label_data(right_node, instrs)
 
 
+def trace_execution(node, instrs):
+    """Mutates instrs to contain only unvisited (unreachable) code"""
+    addr = list(node.keys())[0]
+    instr = lookup_instr_by_addr(addr, instrs)
+    instrs.remove(instr)
+    # Check remaining nodes.
+    left_node = node[list(node.keys())[0]]["LEFT"]
+    right_node = node[list(node.keys())[0]]["RIGHT"]
+    if left_node is not None:
+        trace_execution(left_node, instrs)
+    if right_node is not None:
+        trace_execution(right_node, instrs)
+
+
 def process_data(instrs):
     """Figures out program flow to determine how data is used"""
+    unused_instrs = instrs.copy()
     tree_instrs = instrs.copy()
     btree = build_btree({}, tree_instrs)
+    trace_execution(btree, unused_instrs)
+    for instr in unused_instrs:
+        i = instrs.index(instr)
+        instrs[i][3] = "data"
+        instrs[i][4] = "0"
     label_data(btree, instrs)
     # Final output
     for instr in instrs:
